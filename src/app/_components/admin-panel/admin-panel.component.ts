0d12c8d53f67374a5c70e1f44d962db129945d07
Alberto <alberiba_94@hotmail.com>
2020-03-23 02:40:00 +0100

    datatable en español

--- src/app/_components/admin-panel/admin-panel.component.ts
+++ src/app/_components/admin-panel/admin-panel.component.ts
@@ -7,8 +7,7 @@ import { UserService } from '../../_services/user.service';
   styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent implements OnInit {
-  title2= "<button>"; 
-  title = 'Example of Angular 8 DataTable';
+
   dtOptions: DataTables.Settings = {};
   dtUsers =[
     {"id": 101, "firstName": "Anil", "lastName": "Singh"},
@@ -28,7 +27,30 @@ export class AdminPanelComponent implements OnInit {
 
   ngOnInit(): void {
     this.dtOptions = {
-
+      language: {
+        "decimal":        "",
+        "emptyTable":     "La tabla está vacía",
+        "info":           "Mostrando de _START_ a _END_ de _TOTAL_  registros",
+        "infoEmpty":      "Mostrando 0 de 0 registros",
+        "infoFiltered":   "(fitrados de _MAX_ registros totales)",
+        "infoPostFix":    "",
+        "thousands":      ",",
+        "lengthMenu":     "Registros por página _MENU_ ",
+        "loadingRecords": "Cargando...",
+        "processing":     "Procesando...",
+        "search":         "Filtrar:",
+        "zeroRecords":    "No se han encontrado registros",
+        "paginate": {
+            "first":      "Primero",
+            "last":       "Último",
+            "next":       "Siguiente",
+            "previous":   "Anterior"
+        },
+        "aria": {
+            "sortAscending":  ": Ordenar ascendentemente",
+            "sortDescending": ": Ordenar descendentemente"
+        }
+      },
 
       data:this.dtUsers,
       columns: [{title: 'User ID', data: 'id'},
